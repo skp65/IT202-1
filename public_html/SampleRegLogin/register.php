16eda4db2d16123377a1e80e4b18723345931cb2
shivam <skp65@njit.edu>
2020-06-04 13:43:28 -0400

    WorkInProgress of register form

---
+++ public_html/SampleRegLogin/register.php
@@ -0,0 +1,18 @@
+<form method="POST">
+	<label for="email">Email
+	<input type="email" id="email" name="email"/>
+	</label>
+	<label	for="pass">Password
+	<input type="password" id="pass" name="password"/>
+	</label>
+	<label for="cpass">Confirm Password
+	<input type="password" id="cpasss" name="cpassword"/>
+	</label>
+	<input type="submit" name="register" value="Register"/>
+</form>
+
+<?php
+echo var_export($_GET, true);
+echo var_export($_POST, true);
+echo var_export($_REQUEST, true);
+?>
