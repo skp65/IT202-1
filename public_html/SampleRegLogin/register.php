969cca8d7c43b17a3eedec2ca87354735285ed42
shivam <skp65@njit.edu>
2020-06-04 14:24:37 -0400

    basic reg sample

--- public_html/SampleRegLogin/register.php
+++ public_html/SampleRegLogin/register.php
@@ -17,11 +17,32 @@
 //echo var_export($_REQUEST, true);
 
 if(isset($_POST["register"])){
-	if(isset($_POST["password"]) && isset($_POST["cpassword"])){
+	if(isset($_POST["password"]) && isset($_POST["cpassword"]) && isset($_POST["email"])){
 		$password= $_POST["password"];
 		$cpassword= $_POST["cpassword"];
+		$email = $_POST["email"];
 		if($password == $cpassword){
-			echo "<div>Passwords match</div>";
+			//echo "<div>Passwords match</div>";
+			require("config.php");
+			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+			try{
+				$db = new PDO($connection_string, $dbuser, $dbpass);
+				$stmt = $db->prepare("INSERT INTO USERS (email, password) VALUES(:email, :password)");
+				$stmt->execute(array(
+					":email" => $email,
+					":password" => $password
+				));
+				$e = $stmt->errorInfo();
+				if ($e[0] != "00000"){
+						echo var_export($e, true);
+				}
+				else{
+					echo "<div>Successfully registered! </div>";
+				}
+			}
+			catch (Exception $e){
+				echo $e->getMessage();
+			}
 		}
 		else{
 			echo "<div>Passwords do not match</div>";
