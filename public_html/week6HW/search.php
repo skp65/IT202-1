397f34bb8962631a6c23e3a7e1ba8c12987504a4
shivam <skp65@njit.edu>
2020-06-26 17:41:06 -0400

    search function.

---
+++ public_html/week6HW/search.php
@@ -0,0 +1,42 @@
+<?php
+$search = "";
+if(isset($_POST["search"])){
+    $search = $_POST["search"];
+}
+?>
+    <form method="POST">
+        <input type="text" name="search" placeholder="Search for Product Name"
+               value="<?php echo $search;?>"/>
+        <input type="submit" value="Search"/>
+    </form>
+<?php
+if(isset($search)) {
+
+    require("common.inc.php");
+    if (isset($name) && !empty($name)) {
+        try {
+            $stmt = getDB()->prepare("SELECT * FROM Products where name like CONCAT('%', :name, '%')");
+            $stmt->execute([":name"=>$search]);
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
+?>
+<?php if(isset($results) && count($results) > 0):?>
+    <p>The results are displayed.</p>
+    <ul>
+        <?php foreach($results as $row):?>
+            <li>
+                <?php echo get($row, "name")?>
+                <?php echo get($row, "quantity");?>
+                <?php echo get($row, "price");?>
+                <?php echo get($row, "description");?>
+                <a href="delete.php?thingId=<?php echo get($row, "id");?>">Delete</a>
+            </li>
+        <?php endforeach;?>
+    </ul>
+<?php else:?>
+    <p>No Match Found.</p>
+<?php endif;?>
