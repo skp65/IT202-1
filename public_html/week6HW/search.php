7682a1bca7288969ba809969b3f99708ed8dc8b3
shivam <skp65@njit.edu>
2020-06-26 20:26:13 -0400

    moved a directory

--- public_html/week6HW/search.php
+++ public_html/week6HW/search.php
@@ -33,7 +33,6 @@ if(isset($search)) {
                 <?php echo get($row, "quantity");?>
                 <?php echo get($row, "price");?>
                 <?php echo get($row, "description");?>
-                <a href="delete.php?product_id=<?php echo get($row, "id");?>">Delete</a>
             </li>
         <?php endforeach;?>
     </ul>
