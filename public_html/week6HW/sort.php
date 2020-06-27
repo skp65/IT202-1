9a18be2ba1528679a98cf79efebd53aaf1b32a15
shivam <skp65@njit.edu>
2020-06-27 18:00:32 -0400

    new file added

---
+++ public_html/week6HW/sort.php
@@ -0,0 +1,50 @@
+<?php
+#$search = "";
+if (isset($_POST["search"])) {
+    $search = $_POST["search"];
+}
+?>
+    <form method="POST">
+        <input type="text" name="search" placeholder="Search for Product Name"
+               value="<?php echo $search;?>"/>
+        <input type="submit" value="Search"/>
+    </form>
+<?php
+if (isset($search)) {
+    require("common.inc.php");
+    $sort = $_GET['search'];
+    $query = 'SELECT * FROM Products where name like CONCAT('%', :name, '%') ORDER BY $sort';
+    if (isset($query) && !empty($query)) {
+        try {
+            $stmt = getDB()->prepare($query);
+            $stmt->execute([":name" => $search]);
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
+?>
+
+<?php if (isset($results) && count($results) > 0): ?>
+    <table border="1" cellspacing="2" cellpadding="2">
+        <tr>
+            <th><a href="search.php?search=name">Product Name</a></th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Description</th>
+        </tr>
+    <?php foreach($results as $row):?>
+            <tr>
+                <td><?php echo get($row, "name") ?></td>
+                <td><?php echo get($row, "quantity"); ?></td>
+                <td><?php echo get($row, "price"); ?></td>
+                <td><?php echo get($row, "description");?></td>
+            </tr>
+    <?php endforeach;?>
+    </table>
+<?php else: ?>
+    <p>No Match Found.</p>
+<?php endif; ?>
+
+
