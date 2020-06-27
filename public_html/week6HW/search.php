d40f5e4c376e82470907aa78f6aa052653e327e1
shivam <skp65@njit.edu>
2020-06-26 21:18:58 -0400

    made some changes

--- public_html/week6HW/search.php
+++ public_html/week6HW/search.php
@@ -1,22 +1,22 @@
 <?php
 $search = "";
-if(isset($_POST["search"])){
+if (isset($_POST["search"])) {
     $search = $_POST["search"];
 }
 ?>
     <form method="POST">
         <input type="text" name="search" placeholder="Search for Product Name"
-               value="<?php echo $search;?>"/>
+               value="<?php echo $search; ?>"/>
         <input type="submit" value="Search"/>
     </form>
 <?php
-if(isset($search)) {
+if (isset($search)) {
     require("common.inc.php");
     $query = file_get_contents(__DIR__ . "/query/search.sql");
     if (isset($query) && !empty($query)) {
         try {
             $stmt = getDB()->prepare($query);
-            $stmt->execute([":name"=>$search]);
+            $stmt->execute([":name" => $search]);
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch (Exception $e) {
             echo $e->getMessage();
@@ -24,18 +24,30 @@ if(isset($search)) {
     }
 }
 ?>
-<?php if(isset($results) && count($results) > 0):?>
-    <p>The results are displayed.</p>
-    <ul>
-        <?php foreach($results as $row):?>
-            <li>
-                <?php echo get($row, "name")?>
-                <?php echo get($row, "quantity");?>
-                <?php echo get($row, "price");?>
-                <?php echo get($row, "description");?>
-            </li>
-        <?php endforeach;?>
-    </ul>
-<?php else:?>
+<?php
+    if ($results->num_rows > 0){
+    echo "
+    <table>
+        <tr>
+            <th>Product Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Description</th>
+        </tr>";
+        while($row = $results->fetch_assoc()) {
+        echo "
+        <tr>
+            <td>" . $row["name"]. "</td>
+            <td>" . $row["quantity"]. "</td>
+            <td>" . $row["price"]. "</td>
+            <td>" . $row["description"]. "</td>
+        </tr>;"
+        }
+        echo "</table>";
+    } else {
+    echo "0 results";
+    }
+?>
+<?php else: ?>
     <p>No Match Found.</p>
-<?php endif;?>
+<?php endif; ?>
