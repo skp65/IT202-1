28bade8fb7a50b1a74c0c112561e76ab0ce3806d
shivam <skp65@njit.edu>
2020-07-04 15:20:45 -0400

    Project- fixed some typo

--- public_html/Project/login_out/shop.php
+++ public_html/Project/login_out/shop.php
@@ -38,25 +38,25 @@ if (isset($_POST['code']) && $_POST['code'] != "") {
 }
 
 ?>
-<!DOCTYPE html>
 <html>
 <body>
-    <?php
-    if (!empty($_SESSION["shopping_cart"])) {
-        $cart_count = count(array_keys($_SESSION["shopping_cart"])); ?>
+<div>
+<?php
+if (!empty($_SESSION["shopping_cart"])) {
+$cart_count = count(array_keys($_SESSION["shopping_cart"])); ?>
 
-        <div class="cart_div">
-            <a href="shop.php"><img src="../images/cart.jpg" alt="cart"/> Cart<span>
+<div class="cart_div">
+    <a href="shop.php"><img src="../images/cart.jpg" alt="cart"/> Cart<span>
     <?php echo $cart_count; ?></span></a>
-        </div>
-        <?php
-    }
-    ?>
+</div>
+<?php
+}
+?>
 
-    <?php
-    $result = mysqli_query($connect, "SELECT * FROM `Products`");
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<div class='product-wrapper'>
+<?php
+$result = mysqli_query($connect, "SELECT * FROM `Products`");
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<div class='product-wrapper'>
         <form method='post' action=''>
             <input type='hidden' name='code' value=" . $row['code'] . " />
             <div class='image'><img src='" . $row['image'] . "' /></div>
@@ -65,20 +65,28 @@ if (isset($_POST['code']) && $_POST['code'] != "") {
             <button type='submit' class='buy'>Add to Cart</button>
         </form>
         </div>";
-    }
-    mysqli_close($connect);
-    ?>
-    <?php
+}
+mysqli_close($connect);
+?>
+<?php
 }else{
 ?>
 <?php
-    }
-    ?>
-        <div style="clear:both;"></div>
-
-        <div class="message_box" style="margin:10px 0px;">
-            <?php echo $status; ?>
-        </div>
-
+}
+?>
+<div style="clear:both;"></div>
+<!DOCTYPE html>
+<html>
+<head>
+    <div class="message_box" style="margin:10px 0px;">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <button style="font-size:24px"><i class="fa fa-shopping-cart"></i></button>
+        </head>
+</html>
+<div class="message_box" style="margin:10px 0px;">
+    <?php echo $status; ?>
+</div>
+</div>
 </body>
 </html>
