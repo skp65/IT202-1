17946dcfae61cb28630d495813a5adc212b5ef4c
shivam <skp65@njit.edu>
2020-07-05 16:46:48 -0400

    fixed typos

--- public_html/Project/login_out/changepwd.php
+++ public_html/Project/login_out/changepwd.php
@@ -10,14 +10,17 @@ include("header.php");
             </div>
             <div>
                 <label for="pass">New Password</label><br>
-                <input type="password" id="pass" name="npassword" placeholder="Password" required/>
+                <input type="password" id="pass" name="password" placeholder="Password" required/>
             </div>
             <div>
                 <label for="cpass">Confirm New Password</label><br>
-                <input type="password" id="cpasss" name="cnpassword" placeholder="Confirm Password" required/>
+                <input type="password" id="cpasss" name="cpassword" placeholder="Confirm New Password" required/>
             </div>
             <div>
                 <input class="submit" type="submit" name="register" value="Change Password"/>
+                <input type="button" class="submit"
+                       onclick="window.location.href='https://it202-2020.herokuapp.com/public_html/Project/login_out/login.php'"
+                       value="Go to Login"/>
             </div>
         </form>
     </div>
@@ -28,18 +31,18 @@ include("header.php");
 
 if (isset($_POST["register"])) {
     if (isset($_POST["password"]) && isset($_POST["cpassword"]) && isset($_POST["email"])) {
-        $npassword = $_POST["npassword"];
-        $cnpassword = $_POST["cnpassword"];
+        $password = $_POST["password"];
+        $cpassword = $_POST["cpassword"];
         $email = $_POST["email"];
         if ($password == $cpassword) {
             //echo "<div>Passwords match</div>";
             //require("config.php");
             try {
                 $hash = password_hash($password, PASSWORD_BCRYPT);
-                $stmt = getDB()->prepare("UPDATE Users set password = ':npassword' where email = ':email'");
+                $stmt = getDB()->prepare("UPDATE Users set password = ':password' where email = ':email'");
                 $stmt->execute(array(
                     ':email' => $email,
-                    ':npassword' => $hash //Don't save the raw password $password
+                    ':password' => $hash //Don't save the raw password $password
                 ));
                 $e = $stmt->errorInfo();
                 if ($e[0] != "00000") {
