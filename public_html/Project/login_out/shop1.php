783f43021e002dcdc9da4d2d6f6bfe451721f1d0
shivam <skp65@njit.edu>
2020-07-04 21:45:58 -0400

    made changes

--- public_html/Project/login_out/shop1.php
+++ public_html/Project/login_out/shop1.php
@@ -1,108 +1,166 @@
 <?php
 session_start();
 include('../config1.php');
-$status="";
-if (isset($_POST['code']) && $_POST['code']!=""){
-    $code = $_POST['code'];
-    $result = mysqli_query(getDB(),"SELECT * FROM `Products` WHERE `code`='$code'");
-    $row = mysqli_fetch_assoc($result);
-    $name = $row['name'];
-    $code = $row['code'];
-    $price = $row['price'];
-    $image = $row['image'];
-
-//echo "<BR> " . $name . "<BR>" . $code . "<BR>" . $prince . "<Br>" . $image;
-    $cartArray = array(
-        $code=>array(
-            'name'=>$name,
-            'code'=>$code,
-            'price'=>$price,
-            'quantity'=>1,
-            'image'=>$image)
-    );
-
-    if(empty($_SESSION["shopping_cart"])) {
-        $_SESSION["shopping_cart"] = $cartArray;
-        $status = "<div class='box'  style='color:green; border:1px solid green;padding:15px'>Product is added to your cart!</div>";
-    }else{
-        $array_keys = array_keys($_SESSION["shopping_cart"]);
-        if(in_array($code,$array_keys)) {
-            $status = "<div class='box' style='color:red;border:1px solid red; padding:15px'>
-		Product is already added to your cart!</div>";
-        } else {
-            $_SESSION["shopping_cart"] = array_merge($_SESSION["shopping_cart"],$cartArray);
-            $status = "<div class='box' style='color:green;border: 1px solid green;padding:15px'>Product is added to your cart!</div>";
+if (isset($_POST["add"])){
+    if (isset($_SESSION["cart"])){
+        $item_array_id = array_column($_SESSION["cart"],"product_id");
+        if (!in_array($_GET["id"],$item_array_id)){
+            $count = count($_SESSION["cart"]);
+            $item_array = array(
+                'product_id' => $_GET["id"],
+                'item_name' => $_POST["hidden_name"],
+                'product_price' => $_POST["hidden_price"],
+                'item_quantity' => $_POST["quantity"],
+            );
+            $_SESSION["cart"][$count] = $item_array;
+            echo '<script>window.location="shop1.php"</script>';
+        }else{
+            echo '<script>alert("Product is already Added to Cart")</script>';
+            echo '<script>window.location="shop1.php"</script>';
         }
-
+    }else{
+        $item_array = array(
+            'product_id' => $_GET["id"],
+            'item_name' => $_POST["hidden_name"],
+            'product_price' => $_POST["hidden_price"],
+            'item_quantity' => $_POST["quantity"],
+        );
+        $_SESSION["cart"][0] = $item_array;
     }
 }
 
-
-if(isset($_REQUEST['Logout']) && $_REQUEST['Logout']=="logout")
-{
-    unset($_SESSION['Logged']);
-    header("location:index.php?Msg=You are successfully Logout ");
+if (isset($_GET["action"])){
+    if ($_GET["action"] == "delete"){
+        foreach ($_SESSION["cart"] as $keys => $value){
+            if ($value["product_id"] == $_GET["id"]){
+                unset($_SESSION["cart"][$keys]);
+                echo '<script>alert("Product has been Removed...!")</script>';
+                echo '<script>window.location="shop1.php"</script>';
+            }
+        }
+    }
 }
-
-
-?>
+<!doctype html>
 <html>
 <head>
-    <link rel='stylesheet' href='style.css' type='text/css' media='all' />
-</head>
-<body >
-<div style="width:700px; margin:50 auto;">
-    <?php
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    ?>
-    <form>
-        <button type='submit' name="Logout" value="logout" name="logout" class='logout'>Logout</button>
-    </form>
-    <?php
-    if(!empty($_SESSION["shopping_cart"])) {
-        $cart_count = count(array_keys($_SESSION["shopping_cart"]));
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Titillium+Web');
+
+        *{
+            font-family: 'Titillium Web', sans-serif;
+        }
+        .product{
+            border: 1px solid #eaeaec;
+            margin: -1px 19px 3px -1px;
+            padding: 10px;
+            text-align: center;
+            background-color: #efefef;
+        }
+        table, th, tr{
+            text-align: center;
+        }
+        .title2{
+            text-align: center;
+            color: #66afe9;
+            background-color: #efefef;
+            padding: 2%;
+        }
+        h2{
+            text-align: center;
+            color: #66afe9;
+            background-color: #efefef;
+            padding: 2%;
+        }
+        table th{
+            background-color: #efefef;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container" style="width: 65%">
+        <h2>Shopping Cart</h2>
+        <?php
+            $stmt = getDB()->prepare ("SELECT * FROM Products ORDER BY id");
+            $stmt->execute();
+            $result = $stmt->rowCount();
+            if($result > 0) {
+
+                while ($row = mysqli_fetch_array($result)) {
+
+                    ?>
+                    <div class="col-md-3">
+
+                        <form method="post" action="shop1.php?action=add&id=<?php echo $row["id"]; ?>">
+
+                            <div class="product">
+                                <img src="<?php echo $row["image"]; ?>" class="img-responsive">
+                                <h5 class="text-info"><?php echo $row["name"]; ?></h5>
+                                <h5 class="text-danger"><?php echo $row["price"]; ?></h5>
+                                <input type="text" name="quantity" class="form-control" value="1">
+                                <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>">
+                                <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
+                                <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success"
+                                       value="Add to Cart">
+                            </div>
+                        </form>
+                    </div>
+                    <?php
+                }
+            }
         ?>
-        <div class="cart_div">
-            <a href="cart.php"><meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-                <button style="font-size:24px"><i class="fa fa-shopping-cart"></i></button>
-            </a>
+
+        <div style="clear: both"></div>
+        <h3 class="title2">Shopping Cart Details</h3>
+        <div class="table-responsive">
+            <table class="table table-bordered">
+            <tr>
+                <th width="30%">Product Name</th>
+                <th width="10%">Quantity</th>
+                <th width="13%">Price Details</th>
+                <th width="10%">Total Price</th>
+                <th width="17%">Remove Item</th>
+            </tr>
+
+            <?php
+                if(!empty($_SESSION["cart"])){
+                    $total = 0;
+                    foreach ($_SESSION["cart"] as $key => $value) {
+                        ?>
+                        <tr>
+                            <td><?php echo $value["item_name"]; ?></td>
+                            <td><?php echo $value["item_quantity"]; ?></td>
+                            <td>$ <?php echo $value["product_price"]; ?></td>
+                            <td>
+                                $ <?php echo number_format($value["item_quantity"] * $value["product_price"], 2); ?></td>
+                            <td><a href="shop1.php?action=delete&id=<?php echo $value["product_id"]; ?>"><span
+                                        class="text-danger">Remove Item</span></a></td>
+
+                        </tr>
+                        <?php
+                        $total = $total + ($value["item_quantity"] * $value["product_price"]);
+                    }
+                        ?>
+                        <tr>
+                            <td colspan="3" align="right">Total</td>
+                            <th align="right">$ <?php echo number_format($total, 2); ?></th>
+                            <td></td>
+                        </tr>
+                        <?php
+                    }
+                ?>
+            </table>
         </div>
 
-        <?php
-        echo "<BR><BR>";
-    }
-
-    $result = mysqli_query(getDB(),"SELECT * FROM `Products`");
-    while($row = mysqli_fetch_assoc($result)){
-        echo "<div class='product_wrapper'>
-			  <form method='post' action=''>
-			  <input type='hidden' name='code' value=".$row['code']." />
-			  <div class='image'><img src='".$row['image']."' /></div>
-			  <div class='name'>".$row['name']."</div>
-		   	  <div class='price'>$".$row['price']."</div>
-			  <button type='submit' class='buy'>Buy Now</button>
-			  </form>
-		   	  </div>";
-    }
-    mysqli_close(getDB());
-    ?>
-
-    <div style="clear:both;"></div>
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <div class="message_box" style="margin:10px 0px;">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <button style="font-size:24px"><i class="fa fa-shopping-cart"></i></button>
-            </head>
-    </html>
-
-    <div class="message_box" style="margin:10px 0px;">
-        <?php echo $status; ?>
     </div>
-
-</div>
 </body>
 </html>
