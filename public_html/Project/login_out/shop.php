0b8812eb411717113c918c2f9b2d86fb7fde729e
shivam <skp65@njit.edu>
2020-07-05 21:58:30 -0400

    changed styling

--- public_html/Project/login_out/shop.php
+++ public_html/Project/login_out/shop.php
@@ -4,8 +4,8 @@ include("header.php");
 $status = "";
 if (isset($_POST['code']) && $_POST['code'] != "") {
     $code = $_POST['code'];
-    $stmt = getDB()->prepare ("select * from Products where code= '$code'");
-    $stmt->execute ();
+    $stmt = getDB()->prepare("select * from Products where code= '$code'");
+    $stmt->execute();
     $results = $stmt->fetch(PDO::FETCH_ASSOC);
     $name = $results['name'];
     $code = $results['code'];
@@ -38,43 +38,46 @@ if (isset($_POST['code']) && $_POST['code'] != "") {
 }
 ?>
 <html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
+</head>
 <body>
 <div>
-<?php
-if (!empty($_SESSION["shopping_cart"])) {
-$cart_count = count(array_keys($_SESSION["shopping_cart"]));
-}?>
+    <?php
+    if (!empty($_SESSION["shopping_cart"])) {
+        $cart_count = count(array_keys($_SESSION["shopping_cart"]));
+    } ?>
     <div class="cart_div">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-                <a href="cart.php">
-                    <img src="../images/cart.jpg" style="width: 50px"/>
-                    <span><?php echo $cart_count; ?></span></a>
+        <link rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <a href="cart.php">
+            <img src="../images/cart.jpg" style="width: 50px"/>
+            <span><?php echo $cart_count; ?></span></a>
     </div>
-<?php
-$stmt = getDB()->prepare ("select * from Products");
-$stmt->execute();
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo "<div class='product-wrapper'>
+    <?php
+    $stmt = getDB()->prepare("select * from Products");
+    $stmt->execute();
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "<div class='product-wrapper'>
         <form method='post' action='' >
             <input type='hidden' name='code' value=" . $row['code'] . " />
-            <div class='image'><img src='" . $row['image']."'/></div>
+            <div class='image'><img src='" . $row['image'] . "'/></div>
             <div class='name'>" . $row['name'] . "</div>
             <div class='price'>$" . $row['price'] . "</div>
             <button type='submit' class='buy'><a href='cart.php'></a> Add to Cart</button>
         </form>
         </div>";
-}
-?>
-<div style="clear:both;"></div>
-<!DOCTYPE html>
-    <head>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    </head>
-<div class="message_box" style="margin:10px 0px;">
-    <?php echo $status; ?>
-</div>
+    }
+    ?>
+    <div style="clear:both;"></div>
+    <!DOCTYPE html>
+    <div class="message_box" style="margin:10px 0px;">
+        <?php echo $status; ?>
+    </div>
 </div>
 </body>
 </html>
