efaa1a3e4e0e562f708541add0b6481f1aee2272
shivam <skp65@njit.edu>
2020-07-25 18:03:06 -0400

    WIP-view page

--- public_html/Project/view.php
+++ public_html/Project/view.php
@@ -11,13 +11,22 @@ if (isset($_GET["product_id"])) {
     $stmt->execute([":id" => $product_id]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
-<form method='post' style='text-align: center' action="sort.php?product_id=<?php echo get($result, "id")?>">
+    <!--
+<form method='post' style='text-align: center' action="view.php?product_id=<//?php echo get($result, "id")?>">
     <div class='row'>
         <div class='column'>
-            <img src='<?php echo get($result, "image");?>'/></div>
-        <div class='column'><?php echo get($result, "name");?></div>
-        <div class='column'>$<?php echo get($result, "price");?></div>
-        <div class='column'><?php echo get($result, "description");?></div>
+            <img src='<//?php echo get($result, "image");?>'/></div>
+        <div class='column'><//?php echo get($result, "name");?></div>
+        <div class='column'>$<//?php echo get($result, "price");?></div>
+        <div class='column'><//?php echo get($result, "description");?></div>
     </div>
-</form>
+</form>-->
+    <?php foreach ($result as $row): ?>
+        <tr>
+            <td><img alt="product" src='<?php echo $row["image"]; ?>' width="200px" height="200px"/></td>
+            <td style="text-align: center"><?php echo get($row, "name") ?></td>
+            <td style="text-align: center"><?php echo get($row, "price"); ?></td>
+            <td style="text-align: center"><?php echo get($row, "description"); ?></td>
+        </tr>
+    <?php endforeach; ?>
 <?php } ?>
