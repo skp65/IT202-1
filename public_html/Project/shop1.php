37d28c28db470cb545f0956e7ba931ca9fc6ea56
shivam <skp65@njit.edu>
2020-07-30 17:46:25 -0400

    WIP

--- public_html/Project/shop1.php
+++ public_html/Project/shop1.php
@@ -54,7 +54,7 @@ if (isset($_POST["buy"])) {
             );
         }
     else{?>
-        <b><?php>echo"Login to add items";?></b>
+        <b><?php echo"Login to add items";?></b>
     <?php
     }
 }
