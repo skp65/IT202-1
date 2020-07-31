e7f9ee056fe0bcdf1ec99b24022ac3f801782df9
shivam <skp65@njit.edu>
2020-07-30 21:18:37 -0400

    WIP

--- public_html/Project/shop1.php
+++ public_html/Project/shop1.php
@@ -46,6 +46,8 @@ if (isset($_POST["buy"])) {
     if (isset($_SESSION["user"])) {
         if ($_POST["buy"]) {
             $user_id = $_SESSION["user"]["id"];
+            $product_id = $_GET['pid'];
+            $price = $_GET[$results['price']];
             $stmt = getDB()->prepare("INSERT INTO cart (product_id, quantity, user_id, price) 
             VALUES(:product_id,:quantity, :user_id, :price)");
             $stmt->execute([
