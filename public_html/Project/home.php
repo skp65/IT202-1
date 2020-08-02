30bbd57d9db76a40eb3a7c203fc36ac156f79da2
shivam <skp65@njit.edu>
2020-08-01 21:10:31 -0400

    edit

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,39 +1,24 @@
 <?php
 include_once(__DIR__ . "/partials/header.php");
-?>
-    <div>
-        <h3 style="text-align: center; color: darkorchid; font-weight: bold; font-size: 22px">
-            Welcome to Way of the Food, <?php echo Common::get_username(); ?></h3>
-    </div>
 
-<?php
 if (Common::is_logged_in()) {
     //this will auto redirect if user isn't logged in
 }
-
-$stmt = getDB()->prepare("SELECT * FROM Users where email = 'email'");
-$stmt->execute();
-
-while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    ?>
-    <div>Your Profile</div>
-    <table>
-        <tr>
-            <td>
-                <div>Your name:</div>
-            </td>
-            <td>
-                <?php echo $result['fname'] ?><?php echo $result['lname'] ?>
-            </td>
-            <td>
-                <div>Your Email address:</div>
-            </td>
-            <td>
-                <?php echo $result['email'] ?>
-            </td>
-        </tr>
-    </table>
-    <?php
+?>
+<div>
+    <h3 style="text-align: center; color: darkorchid; font-weight: bold; font-size: 22px">
+        Welcome to Way of the Food, <?php echo Common::get_username(); ?></h3>
+</div>
+<?php
+$user = $_SESSION['user']['id'];
+if ($user){
+    $stmt = getDB()->prepare("select * from Users where id = '$user'");
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo "Your Profile:";
+    $fname = $_SESSION['first_name'];
+    $lname = $_SESSION['last_name'];
+    $email = $_SESSION['email'];
+    echo "<b>Name: $fname . $lname</b>";
+    echo "<b>Email: $email</b>";
 }
 ?>
-
