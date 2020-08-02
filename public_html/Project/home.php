7e58efb894bab7a98506b4c963d76c6aa50f3894
shivam <skp65@njit.edu>
2020-08-01 20:11:26 -0400

    edit

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -9,8 +9,8 @@ if (Common::is_logged_in()) {
         Welcome to Way of the Food, <?php echo Common::get_username(); ?></h3>
 </div>
 <?php
-$id = $_SESSION["user"]["id"];
-$stmt = getDB()->prepare("SELECT * FROM Users where id = '$id'");
+
+$stmt = getDB()->prepare("SELECT * FROM Users where email = 'email' and fname = 'fname' and lname ='lname'");
 $stmt->execute();
 
 while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -22,13 +22,13 @@ while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 <div>Your name:</div>
             </td>
             <td>
-                <?php echo$result['fname']?> <?php echo$result['lname']?>
+                <?php echo $result['fname'] ?><?php echo $result['lname'] ?>
             </td>
             <td>
                 <div>Your Email address:</div>
             </td>
             <td>
-                <?php echo$result['email']?>
+                <?php echo $result['email'] ?>
             </td>
         </tr>
     </table>
