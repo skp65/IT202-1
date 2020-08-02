bddd600f175171e2e4c70bf7812aa880d141d1f8
shivam <skp65@njit.edu>
2020-08-01 20:15:08 -0400

    edit

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,14 +1,13 @@
+<div>
+    <h3 style="text-align: center; color: darkorchid; font-weight: bold; font-size: 22px">
+        Welcome to Way of the Food, <?php echo Common::get_username(); ?></h3>
+</div>
+
 <?php
 include_once(__DIR__ . "/partials/header.php");
 if (Common::is_logged_in()) {
     //this will auto redirect if user isn't logged in
 }
-?>
-<div>
-    <h3 style="text-align: center; color: darkorchid; font-weight: bold; font-size: 22px">
-        Welcome to Way of the Food, <?php echo Common::get_username(); ?></h3>
-</div>
-<?php
 
 $stmt = getDB()->prepare("SELECT * FROM Users where email = 'email' and fname = 'fname' and lname ='lname'");
 $stmt->execute();
