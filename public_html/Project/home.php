be3e7b2be43ae0acf48bb1249e68eb113463e349
shivam <skp65@njit.edu>
2020-08-01 21:16:52 -0400

    edit

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -15,10 +15,7 @@ if ($user){
     $stmt = getDB()->prepare("select * from Users where id = '$user'");
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     echo "Your Profile:";
-    $fname = $_SESSION['first_name'];
-    $lname = $_SESSION['last_name'];
-    $email = $_SESSION['email'];
-    echo "<b>Name: $fname . $lname</b>";
-    echo "<b>Email: $email</b>";
+    echo "Name:" . $result['first_name'] . $result['last_name'];
+    echo "Email:" .$result['email'];
 }
 ?>
