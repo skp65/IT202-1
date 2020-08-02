ae5f31a803faf9366d37122641819ecf04691ec9
shivam <skp65@njit.edu>
2020-08-01 20:17:04 -0400

    edit

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,15 +1,17 @@
-<div>
-    <h3 style="text-align: center; color: darkorchid; font-weight: bold; font-size: 22px">
-        Welcome to Way of the Food, <?php echo Common::get_username(); ?></h3>
-</div>
-
 <?php
 include_once(__DIR__ . "/partials/header.php");
+?>
+    <div>
+        <h3 style="text-align: center; color: darkorchid; font-weight: bold; font-size: 22px">
+            Welcome to Way of the Food, <?php echo Common::get_username(); ?></h3>
+    </div>
+
+<?php
 if (Common::is_logged_in()) {
     //this will auto redirect if user isn't logged in
 }
 
-$stmt = getDB()->prepare("SELECT * FROM Users where email = 'email' and fname = 'fname' and lname ='lname'");
+$stmt = getDB()->prepare("SELECT * FROM Users where email = 'email'");
 $stmt->execute();
 
 while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
